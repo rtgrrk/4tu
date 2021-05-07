775bf9832d9aaeeda43042ec53637b9e789905f8
Rutger Rek <rutgerrek@hotmail.com>
2021-05-07 13:17:43 +0200

    modified static config

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Assignment 4TU
+
+## Installation
+docker-compose build
+docker-compose up
+
+docker exec python ./manage.py collectstatic --no-input
--- app/tu/settings.py
+++ app/tu/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'tud/app', 'static'),
+    os.path.join(BASE_DIR, 'tu', 'static'),
 ]
 
 # settings cors headers
