700d2aa3f3f746f99df5fcdeae080b0b5fdbf2d6
Rutger Rek <rutgerrek@hotmail.com>
2021-05-11 11:06:07 +0200

    .gitignore

---
+++ app/tu/settings_local.py
@@ -0,0 +1,16 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DEBUG = True
+
+# ALLOWED_HOSTS = ['localhost', '10.0.2.2']
+ALLOWED_HOSTS = [*]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
