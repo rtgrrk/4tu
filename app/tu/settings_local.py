58bc3184c4b39a4fb580e10f86f1799663c73338
Rutger Rek <rutgerrek@hotmail.com>
2021-05-11 11:09:51 +0200

    added * to allow_localhosts

--- app/tu/settings_local.py
+++ app/tu/settings_local.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEBUG = True
 
 # ALLOWED_HOSTS = ['localhost', '10.0.2.2']
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
